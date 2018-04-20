19e91834f7a96a9b419763601eb7a89a3066ad29
Darryl L. Pierce <mcpierce@gmail.com>
2018-04-20 18:21:30 -0400

    [Issue #31] A list of comics can be loaded and imported.

--- web-frontend/src/app/comic/comic.module.ts
+++ web-frontend/src/app/comic/comic.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { ComicRoutingModule } from './comic-routing.module';
 import { ComicListComponent } from './comic-list/comic-list.component';
@@ -8,7 +9,9 @@ import { ImportComicsComponent } from './import-comics/import-comics.component';
 @NgModule({
   imports: [
     CommonModule,
-    ComicRoutingModule
+    ComicRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   declarations: [ComicListComponent, ImportComicsComponent]
 })
